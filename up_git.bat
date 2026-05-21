6ad008b7d19b81dd8204f5cda464100f335916a4
kendrick3004 <101604411+kendrick3004@users.noreply.github.com>
2026-05-21 12:58:46 -0300

    Auto update

--- up_git.bat
+++ up_git.bat
@@ -5,6 +5,14 @@ echo ============================================
 echo Atualizacao de arquivos em andamento...
 echo ============================================
 
+:: Cria .gitkeep em pastas vazias
+for /f "delims=" %%d in ('dir /ad /b /s') do (
+    dir /a /b "%%d" >nul 2>&1
+    if errorlevel 1 (
+        type nul > "%%d\.gitkeep"
+    )
+)
+
 git add -A
 
 git diff --cached --quiet
@@ -19,4 +27,3 @@ echo ============================================
 echo FINALIZADO - Atualizacao de arquivos concluida.
 echo ============================================
 pause
-
